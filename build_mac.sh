544bc5add32a497bb3dcf24ab8f1ba737bbffdcf
Alexander Skvortsov <askvortsov@yandex-team.ru>
2018-01-12 17:55:54 +0300

    Replaced buildscript

--- build_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-swift build -c release -Xswiftc -static-stdlib
---
+++ build_thread_safe.swift
@@ -0,0 +1,29 @@
+#!/usr/bin/swift
+
+import Foundation
+
+func perform(_ command: String) {
+  perform(command.components(separatedBy: " "))
+}
+
+func perform(_ command: [String]) {
+  guard !command.isEmpty else {
+    return
+  }
+
+  let task = Process()
+  task.launchPath = command[0]
+  task.arguments = Array(command.dropFirst())
+  task.environment = ProcessInfo.processInfo.environment
+  task.launch()
+  task.waitUntilExit()
+}
+
+try? FileManager().createDirectory(atPath: ".build",
+                                   withIntermediateDirectories: false)
+
+let fd = fopen(".build/.build_lock", "w")!
+lockf(Int32(fd.pointee._file), F_LOCK, 0)
+perform("/usr/bin/swift build -c release -Xswiftc -static-stdlib --product cggen")
+lockf(Int32(fd.pointee._file), F_ULOCK, 0)
+fclose(fd)
