237c14d7c4a1934371429d8e60bec1ba5cf05193
Daniil_F <dan4fedor@gmail.com>
2021-04-09 18:43:12 +0300

    Added tests that do show a lot of red( - also fixed CE after swiftformat

---
+++ Tests/RegressionTests/BCTests.swift
@@ -0,0 +1,118 @@
+import CoreGraphics
+import Foundation
+import XCTest
+
+import BCRunner
+import libcggen
+
+private let defTolerance = 0.002
+private let defScale = 2.0
+
+let PDFsamplesPath = getCurentFilePath().appendingPathComponent("pdf_samples")
+
+class BCPDFTests: XCTestCase {
+  func testAlpha() {
+    testBC(pdf: "alpha")
+  }
+
+  func testCapsJoins() {
+    testBC(pdf: "caps_joins")
+  }
+
+  func testDashes() {
+    testBC(pdf: "dashes")
+  }
+
+  func testFill() {
+    testBC(pdf: "fill")
+  }
+
+  func testGradient() {
+    testBC(pdf: "gradient")
+  }
+
+  func testGradientRadial() {
+    testBC(pdf: "gradient_radial")
+  }
+
+  func testGradientShape() {
+    testBC(pdf: "gradient_shape")
+  }
+
+  func testGradientThreeDots() {
+    testBC(pdf: "gradient_three_dots")
+  }
+
+  func testGradientWithAlpha() {
+    testBC(pdf: "gradient_with_alpha")
+  }
+
+  func testGradientWithMask() {
+    testBC(pdf: "gradient_with_mask")
+  }
+
+  func testGroupOpacity() {
+    testBC(pdf: "group_opacity")
+  }
+
+  func testLines() {
+    testBC(pdf: "lines")
+  }
+
+  func testNestedTransparentGroup() {
+    testBC(pdf: "nested_transparent_group", tolerance: 0.003)
+  }
+
+  func testShapes() {
+    testBC(pdf: "shapes", tolerance: 0.004)
+  }
+
+  func testUnderlyingObjectWithTinyAlpha() {
+    testBC(pdf: "underlying_object_with_tiny_alpha")
+  }
+
+  func testWhiteCrossScnOperator() {
+    testBC(pdf: "white_cross_scn_operator")
+  }
+}
+
+func testBC(
+  pdf: String,
+  tolerance: Double = defTolerance,
+  scale: Double = defScale
+) {
+  let url = PDFsamplesPath.appendingPathComponent(pdf)
+    .appendingPathExtension("pdf")
+  XCTAssertNoThrow(try testBC(path: url, tolerance: tolerance, scale: scale))
+}
+
+func testBC(
+  path: URL,
+  tolerance: Double = defTolerance,
+  scale: Double = defScale
+) throws {
+  // TODO: Add support for svg
+
+  let reference = try renderPDF(from: path, scale: CGFloat(defScale))
+  let bytecode = try getBytecode(from: path)
+  let cs = CGColorSpaceCreateDeviceRGB()
+  guard let context = CGContext(
+    data: nil,
+    width: Int(50 * scale),
+    height: Int(50 * scale),
+    bitsPerComponent: 8,
+    bytesPerRow: 0,
+    space: cs,
+    bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+  ) else {
+    throw Err("Failed to create CGContext")
+  }
+  context
+    .concatenate(CGAffineTransform(scaleX: CGFloat(scale), y: CGFloat(scale)))
+  try runBytecode(context, fromData: Data(bytecode))
+  guard let result = context.makeImage() else {
+    throw Err("Failed to draw CGImage")
+  }
+  let diff = compare(reference, result)
+  XCTAssertLessThan(diff, tolerance)
+}
